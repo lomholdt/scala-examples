251c519c6b52f5ed57c52f209b29e2d79e0be87b
lomholdt <jonaslomholdt@gmail.com>
2016-01-17 21:20:55 +0100

    Added tests and some comments

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,51 +1,49 @@
-
+import scala.annotation.tailrec
 
 /**
- * Object with List methods in 
- * different variations
+ * Object with typical List methods 
+ * in different variations.
  * 
- * @author Jonas Lomholdt
- * @date 17/01-2016
+ * @author 		Jonas Lomholdt
+ * @date        2016-01-17
+ * @timestamp 	2016-01-17T21:14:13+0100
  */
 object MyList {
 	
-	/**
-	 * map
-	 */
-	
+	// map using pattern matching and recursion 
 	def map[A,B](l: List[A])(f: A => B): List[B] = l match {
 		case Nil => Nil
 		case h::t => f(h) :: map(t)(f)
 	}
 
+	// map using pattern matching and wildcard basecase
 	def map2[A,B](l: List[A])(f: A => B): List[B] = l match {
-		case h::t => f(h) :: map(t)(f)
+		case h::t => f(h) :: map2(t)(f)
 		case _ => Nil
 	}
-
-
-	/**
-	 * flatMap
-	 */
 	
+	// flatMap using build-in flatten method
+	// no pattern matching
 	def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = {
 		map(l)(f).flatten
 	}	
 
+	// recursive flatMap without use of builtin methods
+	// using pattern matching
 	def flatMap2[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
 		case Nil => Nil
-		case h::t => f(h) ++ flatMap(t)(f)
+		case h::t => f(h) ++ flatMap2(t)(f)
 	}
 
+	// 
 	def flatMap3[A,B](l: List[A])(f: A => List[B]): List[B] = {
 		l.foldLeft(List[B]())(_ ++ f(_))
-		
 	}
 
-	def flapMap4[A,B](l: List[A])(f: A => List[B]): List[B] = {
-		// Closure function
+	def flatMap4[A,B](l: List[A])(f: A => List[B]): List[B] = {
+		@tailrec
 		def go(l: List[A], r: List[B])(f: A => List[B]): List[B] = l match {
-			case Nil => Nil
+			case Nil => r
 			case h::t => go(t, r ++ f(h))(f)
 		}
 		go(l, List[B]())(f)
---
+++ src/test/scala/ListSpec.scala
@@ -0,0 +1,41 @@
+
+import org.scalatest.FlatSpec
+
+class ListSpec extends FlatSpec {
+
+	import MyList._
+
+	val a = List(1,2,3,4,5)
+	val a1 = List(2,3,4,5,6)
+
+	val b = List(List(1),List(2),List(3),List(4),List(5))
+	val b1 = List(1,2,3,4,5)
+
+	
+	/**
+	 * map
+	 */
+	it should "(map) increment every element by one" in {
+		assert(map(a)(a => a+1) == a1)
+	}	
+	it should "(map2) increment every element by one" in {
+		assert(map2(a)(a => a+1) == a1)
+	}
+
+	/**
+	 * flatMap
+	 */
+	it should "(flatMap) every element" in {
+		assert(flatMap(b)(b => b) == b1)
+	}
+	it should "(flatMap2) every element" in {
+		assert(flatMap2(b)(b => b) == b1)
+	}	
+	it should "(flatMap3) every element" in {
+		assert(flatMap3(b)(b => b) == b1)
+	}		
+	it should "(flatMap4) every element" in {
+		assert(flatMap4(b)(b => b) == b1)
+	}	
+	
+}
