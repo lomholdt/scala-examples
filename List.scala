7a7f82b61869c5b33a7bee9128ec99a77975df38
lomholdt <jonaslomholdt@gmail.com>
2016-01-17 20:13:16 +0100

    Initial commit

---
+++ List.scala
@@ -0,0 +1,46 @@
+
+
+/**
+ * Object with List methods in 
+ * different variations
+ */
+object MyList {
+	
+	
+	def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+		case Nil => Nil
+		case h::t => f(h) :: map(t)(f)
+	}
+
+	def map2[A,B](l: List[A])(f: A => B): List[B] = l match {
+		case h::t => f(h) :: map(t)(f)
+		case _ => Nil
+	}
+
+	def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = {
+		map(l)(f).flatten
+	}	
+
+	def flatMap2[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
+		case Nil => Nil
+		case h::t => f(h) ++ flatMap(t)(f)
+	}
+
+	def flatMap3[A,B](l: List[A])(f: A => List[B]): List[B] = {
+		l.foldLeft(List[B]())(_ ++ f(_))
+		
+	}
+
+	def flapMap4[A,B](l: List[A])(f: A => List[B]): List[B] = {
+		// Closure function
+		def go(l: List[A], r: List[B])(f: A => List[B]): List[B] = l match {
+			case Nil => Nil
+			case h::t => go(t, r ++ f(h))(f)
+
+		}
+
+	}
+
+
+	
+}
